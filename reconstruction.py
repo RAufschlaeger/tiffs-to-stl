c349b64286483679a61a2a173e19b21041be2fd6
robertaufschlager <aufsch05@gw.uni-passau.de>
2020-09-09 23:27:35 +0200

    Add triangulation code for merging two adjacent slices and loop which combines all merging results in one stl file. For now just z-direction is scanned. x- and y- directions still have to be implemented

--- reconstruction.py
+++ reconstruction.py
@@ -5,19 +5,23 @@ import numpy as np
 from stl import mesh
 from mpl_toolkits import mplot3d
 
+IMAGE_SIZE = 227
 
-def triangles(img: str, level: int, orientation: str) -> list:
+
+def triangles(img, level: int, orientation: str) -> list:
     """
     :param img: name of a black and white image in the slices folder
     :param level: level of the sliced image
     :param orientation: one of six directions
     :return: facets: triangulation of img
     """
-    im = Image.open('slices/'+img)
-    img = np.array(im)
+    if type(img) == str:
+        im = Image.open('slices/'+img)
+        img = np.array(im)
+
     facets = []
-    for i in range(0, 227):
-        for j in range(0, 227):
+    for i in range(0, IMAGE_SIZE):
+        for j in range(0, IMAGE_SIZE):
             if img[i][j] == 0:  # if pixel is black
                 if orientation == 'z-':  # from below
                     # traverse clock-wise
@@ -56,32 +60,70 @@ def get_roof(images: list) -> list:
 
 
 def x_scan(img1, img2) -> list:
-    return
+    return []
 
 
 def y_scan(img1, img2) -> list:
-    return
+    return []
 
 
-def z_scan(img1, img2) -> list:
-    return
+def z_scan(L: str, U: str, level: int) -> list:
+    """
+    :param L: title of lower image
+    :param U: title of upper image
+    :return: z-direction facets resulting from two slice layers
+    """
+
+    # create images for upper and lower contour in intermediate layer
+    roof = Image.new('P', (IMAGE_SIZE, IMAGE_SIZE), 255)
+    roof = np.array(roof)
+    bottom = Image.new('P', (IMAGE_SIZE, IMAGE_SIZE), 255)
+    bottom = np.array(bottom)
+
+    # load slices
+    L = Image.open('slices/'+L)
+    L = np.array(L)
+    U = Image.open('slices/' + U)
+    U = np.array(U)
+
+    for i in range(0, IMAGE_SIZE):
+        for j in range(0, IMAGE_SIZE):
+            if L[i][j] == 0 and U[i][j] == 255:
+                roof[i][j] = 0
+            if U[i][j] == 0 and L[i][j] == 255:
+                bottom[i][j] = 0
+
+    return triangles(roof, level, 'z+') + triangles(bottom, level, 'z-')
 
 
-def merge(img1, img2) -> list:
-    triangulation = [x_scan(img1, img2), y_scan(img1, img2), z_scan(img1, img2)]
-    return triangulation
+def merge(img1: str, img2: str, level: int) -> list:
+    """
+    :param img1: title of lower image
+    :param img2: title of upper image
+    :return facets: facets resulting from two slice layers
+    """
+    # ToDo: implement x and y scan procedure in merging process
+    facets = x_scan(img1, img2) + y_scan(img1, img2) + z_scan(img1, img2, level)
+    return facets
 
 
 def triangulate(images: list) -> list:
     """
     :param images: a (ordered) list of images
-    :return: triangulation: triangulation of images as list of facets
+    :return: facets: facets of all slices
     """
-    n = len(images)
-    triangulation = [get_bottom(images), get_roof(images)]  # unordered list of facets
-    for level in range(0, n-2):
-        triangulation.append(merge(images[level], images[level + 1]))
-    return triangulation
+
+    facets = []
+    for level in range(0, len(images) - 1):
+        level_facets = merge(images[level], images[level + 1], level + 1)
+        facets += level_facets
+
+    roof_facets = get_roof(images)
+    bottom_facets = get_bottom(images)
+
+    facets = bottom_facets + facets + roof_facets
+    facets = np.array(facets)
+    return facets
 
 
 # load images: list
@@ -94,18 +136,17 @@ images.sort()
 
 # create stl file:
 
-roof_facets = get_roof(images)
-bottom_facets = get_bottom(images)
-facets = bottom_facets + roof_facets
-facets = np.array(facets)
+facets = triangulate(images)
 
-# ToDo:
-#  1. merge first and second level: facets = merge(images[0],images[1])
-#  2. generalize for all levels, i.e. create stl from all images: triangulation = triangulate(images)
+# to test merge:
+# facets = merge(images[0], images[1], 1)
 
 # create the mesh
 level = mesh.Mesh(np.zeros(facets.shape[0], dtype=mesh.Mesh.dtype))
 level.vectors = facets
+
+# scale layer height
+level.vectors[:,:, 2] *= 4  # ToDo: check, if 4 is correct!
 
 # write the mesh to file "level.stl"
 level.save('level.stl')
