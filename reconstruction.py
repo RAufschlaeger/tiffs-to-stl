e47ea2a080b5e7d4625a7eee9f378d31a51ace89
robertaufschlager <aufsch05@gw.uni-passau.de>
2020-09-26 13:45:44 +0200

    Add scans for x- and y-direction. This completes the scan.

--- reconstruction.py
+++ reconstruction.py
@@ -4,15 +4,16 @@ import os
 import numpy as np
 from stl import mesh
 from mpl_toolkits import mplot3d
+import math
 
 IMAGE_SIZE = 57  # 227
 SLICES_DIR_Z = 'slices_z/'
-# SLICES_DIR_X = 'slices_x/'
-# SLICES_DIR_Y = 'slices_y/'
+SLICES_DIR_X = 'slices_x/'
+SLICES_DIR_Y = 'slices_y/'
 # PLOT_DIR = 'plot/'
 
 
-def triangles(img, level: int, orientation: str) -> list:
+def triangles(img, level: int, orientation: str, dir: str) -> list:
     """
     :param img: name of a black and white image in the slices folder
     :param level: level of the sliced image
@@ -20,7 +21,7 @@ def triangles(img, level: int, orientation: str) -> list:
     :return: facets: triangulation of img
     """
     if type(img) == str:
-        im = Image.open(SLICES_DIR_Z+img)
+        im = Image.open('slices_' + dir + '/' + img)
         img = np.array(im)
 
     facets = []
@@ -36,40 +37,33 @@ def triangles(img, level: int, orientation: str) -> list:
                     facets.append([[i, j, level], [i + 1, j + 1, level], [i, j + 1, level]])
                 else:
                     break
-                '''
-                elif orientation == 'x-':  # from here
-                    break
-                elif orientation == 'x+':  # from there
-                    break
-                elif orientation == 'y-':  # from left
-                    break
-                elif orientation == 'y+':  # from right
-                    break
-                '''
     return facets
 
 
-def get_bottom(images: list) -> list:
+def get_bottom(images: list, dir: str) -> list:
     """
+    :param dir: direction
     :param images: a (ordered) list of images
     :return: facets of first image in images
     """
-    return triangles(images[0], 0, 'z-')
+    return triangles(images[0], 0, 'z-', dir)
 
 
-def get_roof(images: list) -> list:
+def get_roof(images: list, dir: str) -> list:
     """
+    :param dir: direction
     :param images: A (ordered) list of images
     :return: triangulation of last image in images
     """
-    return triangles(images[len(images) - 1], len(images), 'z+')
+    return triangles(images[len(images) - 1], len(images), 'z+', dir)
 
 
-def merge(img1: str, img2: str, level: int) -> list:
+def merge(img1: str, img2: str, level: int, dir: str) -> list:
     """
     :param img1: title of lower image
     :param img2: title of upper image
     :param level: level of layer
+    :param dir: direction of layer
     :return facets: facets resulting from two slice layers
     """
 
@@ -80,21 +74,22 @@ def merge(img1: str, img2: str, level: int) -> list:
     bottom = np.array(bottom)
 
     # load slices
-    L = np.array(Image.open(SLICES_DIR_Z + img1))
-    U = np.array(Image.open(SLICES_DIR_Z + img2))
+    L = np.array(Image.open('slices_' + dir + '/' + img1))
+    U = np.array(Image.open('slices_' + dir + '/' + img2))
 
-    for i in range(0, IMAGE_SIZE-1):
-        for j in range(0, IMAGE_SIZE-1):
+    for i in range(0, IMAGE_SIZE - 1):
+        for j in range(0, IMAGE_SIZE - 1):
             if L[i][j] == 0 and U[i][j] == 255:
                 roof[i][j] = 0
             if U[i][j] == 0 and L[i][j] == 255:
                 bottom[i][j] = 0
 
-    return triangles(roof, level, 'z+') + triangles(bottom, level, 'z-')
+    return triangles(roof, level, 'z+', dir) + triangles(bottom, level, 'z-', dir)
 
 
-def triangulate_z(images: list) -> list:
+def triangulate_z(images: list, dir: str) -> list:
     """
+    :param dir: direction
     :param images: a (ordered) list of images
     :param orientation: orientation of the scan
     :return: facets: facets of all slices
@@ -102,10 +97,10 @@ def triangulate_z(images: list) -> list:
 
     facets = []
     for level in range(0, len(images) - 1):  # len(images) - 1
-        level_facets = merge(images[level], images[level + 1], level + 1)
+        level_facets = merge(images[level], images[level + 1], level + 1, dir)
         facets += level_facets
-    roof_facets = get_roof(images)
-    bottom_facets = get_bottom(images)
+    roof_facets = get_roof(images, dir)
+    bottom_facets = get_bottom(images, dir)
     facets = facets + roof_facets + bottom_facets
     facets = np.array(facets)
 
@@ -120,27 +115,52 @@ for file in os.listdir(img_dir):
         images_z.append(file)
 images_z = sorted(images_z)
 
+img_dir = SLICES_DIR_X
+images_x = []  # list of strings
+for file in os.listdir(img_dir):
+    if file.endswith("tif"):
+        images_x.append(file)
+images_x = sorted(images_x)
+
+img_dir = SLICES_DIR_Y
+images_y = []  # list of strings
+for file in os.listdir(img_dir):
+    if file.endswith("tif"):
+        images_y.append(file)
+images_y = sorted(images_y)
+
 # ToDo: same for images_x and images_y
 
 # create stl file:
 
-facets_z = triangulate_z(images_z)
-# facets_x = triangulate_x(images_x)
-# facets_y = triangulate_y(images_y)
-
-facets = facets_z  # + facets_x + facets_y
+facets_z = triangulate_z(images_z, 'z')
+facets_x = triangulate_z(images_x, 'x')
+facets_y = triangulate_z(images_y, 'y')
 
 # to test merge:
 # facets = merge(images[0], images[1], 1)
 
 # create the mesh
-level = mesh.Mesh(np.zeros(facets.shape[0], dtype=mesh.Mesh.dtype))
-level.vectors = facets
+level_1 = mesh.Mesh(np.zeros(facets_z.shape[0], dtype=mesh.Mesh.dtype))
+level_1.vectors = facets_z
+level_2 = mesh.Mesh(np.zeros(facets_x.shape[0], dtype=mesh.Mesh.dtype))
+level_2.vectors = facets_x
+level_2.rotate(axis=[0, 0.5, 0], theta=math.radians(270))
+level_2.vectors[:, :, 2] = - level_2.vectors[:, :, 2]
+level_2.vectors[:, [0, 1, 2], :] = level_2.vectors[:, [2, 1, 0], :]
+level_3 = mesh.Mesh(np.zeros(facets_y.shape[0], dtype=mesh.Mesh.dtype))
+level_3.vectors = facets_y
+level_3.rotate(axis=[0.5, 0, 0], theta=math.radians(270))
+level_3.vectors[:, :, 1] = - level_3.vectors[:, :, 1]
+level_3.vectors[:, [0, 1, 2], :] = level_3.vectors[:, [2, 1, 0], :]
 
 # scale layer height
 # level.vectors[:, :, 2] *= 4  # ToDo: check, if 4 is correct!
 
 # write the mesh to file "object.stl"
+level = mesh.Mesh(np.zeros(facets_z.shape[0] + facets_x.shape[0] + facets_y.shape[0], dtype=mesh.Mesh.dtype))
+one_and_two_vectors = np.append(level_1.vectors, level_2.vectors, axis=0)
+level.vectors = np.append(one_and_two_vectors, level_3.vectors, axis=0)
 level.save('object.stl')
 
 # plot:
@@ -163,5 +183,5 @@ axes.auto_scale_xyz(scale, scale, scale)
 plt.show()
 
 # for i in range(0, 360, 30):
-    # axes.view_init(elev=30, azim=i)
-    # figure.savefig(PLOT_DIR + 'plot%d.png' % i)
+# axes.view_init(elev=30, azim=i)
+# figure.savefig(PLOT_DIR + 'plot%d.png' % i)
